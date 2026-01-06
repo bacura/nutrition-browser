270ac7db0fc242b34b34826a074e27084ab2e71c
noritama <noritama@x.bacura.jp>
2026-01-06 21:09:17 +0900

    update

--- nb2020-export.rb
+++ nb2020-export.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/ruby
 #encoding: utf-8
-#Nutrition browser 2020 export 0.0.2 (2025/12/28)
+#Nutrition browser 2020 export 0.1.0 (2026/01/04)
 
 #==============================================================================
 #STATIC
@@ -66,9 +66,40 @@ when 'memory'
 	r.each do |e| export << "#{e['user']}\t#{e['category']}\t#{e['pointer']}\t#{e['memory']}\t#{e['rank']}\t#{e['total_rank']}\t#{e['count']}t#{e['know']}\t#{e['date']}\n" end
 	puts "NB2020 [memory] data #{@date}\n"
 	puts export.force_encoding( 'UTF-8' )
+
+when 'fctp'
+	export = ''
+	r = $DB.query( "SELECT * FROM #{$TB_FCTP} WHERE user='#{ARGV[1]}';" )
+	r.each do |e|
+		export << "#{e['user']}"
+		@fct_item.each do |item| export << "\t#{e[item]}" end
+		export << "\n"
+	end
+	puts "NB2020 [fctp] data (#{ARGV[1]}) #{@date}\n"
+	items = "user"
+	@fct_item.each do |item| items << "\t#{item}" end
+	puts items + "\n"
+	puts export.force_encoding( 'UTF-8' )
+
+when 'tagp'
+	export = ''
+	r = $DB.query( "SELECT * FROM #{$TB_TAG} INNER JOIN #{$TB_FCTP} ON #{$TB_TAG}.FN = #{$TB_FCTP}.FN WHERE #{$TB_TAG}.user='#{ARGV[1]}';" )
+	r.each do |e| export << "#{e['FG']}\t#{e['FN']}\t#{e['SID']}\t#{e['SN']}\t#{e['user']}\t#{e['name']}\t#{e['class1']}\t#{e['class2']}\t#{e['class3']}\t#{e['tag1']}\t#{e['tag2']}\t#{e['tag3']}\t#{e['tag4']}\t#{e['tag5']}\t#{e['status']}\n" end
+	puts "NB2020 [tagp] data (#{ARGV[1]}) #{@date}\n"
+	puts "FG\tFN\tSID\tSN\tuser\tname\tclass1\tclass2\tclass3\ttag1\ttag2\ttag3\ttag4\ttag5\tstatus\n"
+	puts export.force_encoding( 'UTF-8' )
+
+when 'extp'
+	export = ''
+	r = $DB.query( "SELECT * FROM #{$TB_EXT} INNER JOIN #{$TB_FCTP} ON #{$TB_EXT}.FN = #{$TB_FCTP}.FN WHERE #{$TB_EXT}.user='#{ARGV[1]}';" )
+	r.each do |e| export << "#{e['FN']}\t#{e['user']}\t#{e['gycv']}\t#{e['allergen1']}\t#{e['allergen2']}\t#{e['unit']}\t#{e['color1']}\t#{e['color2']}\t#{e['color1h']}\t#{e['color2h']}\t#{e['shun1s']}\t#{e['shun1e']}\t#{e['shun2s']}\t#{e['shun2e']}\n" end
+	puts "NB2020 [extp] data (#{ARGV[1]}) #{@date}\n"
+	puts "FN\tuser\tgycv\tallergen1\tallergen2\tunit\tcolor1\tcolor2\tcolor1h\tcolor2h\tshun1s\tshun1e\tshun2s\tshun2e\n"
+	puts export.force_encoding( 'UTF-8' )
+
 else
 	puts 'Nutrition browser 2020 export 0.0.2 (2024/10/14)'
-	puts '[Usage]ruby nb2020-export.rb Exportable data > nb2020-dic.txt'
+	puts '[Usage]ruby nb2020-export.rb Exportable data [user]> nb2020-dic.txt'
 	puts 'Exportable data list..'
 	puts 'unit'
 	puts 'gycv'
@@ -76,4 +107,7 @@ else
 	puts 'allergen'
 	puts 'dic'
 	puts 'memory'
+	puts 'fctp'
+	puts 'tagp'
+	puts 'extp'
 end
--- nb2020-import.rb
+++ nb2020-import.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/ruby
 #encoding: utf-8
-#Nutrition browser 2020 import 0.0.0 (2024/10/14)
+#Nutrition browser 2020 import 0.1.0 (2024/10/14)
 
 #==============================================================================
 #STATIC
@@ -24,8 +24,10 @@ require './nb2020-soul'
 
 #### data loading
 import_solid = []
+items = []
 txt_class = ''
 line1_flag = true
+line2_flag = false
 
 opt = ARGV[0]
 data_file = ARGV[1]
@@ -49,19 +51,24 @@ File.open( data_file, 'r' ) do |f|
 	f.each_line do |line|
 		if line1_flag
 			a = line.chomp.split( "\s" )
-			if a[0] == 'NB2020' && /^\[\w+\]$/ =~ a[1] && a[2] == 'data'
+			if a[0] == 'NB2020' && /^\[\w+\]$/ =~ a[1]
 				txt_class = a[1].sub( '[', '' ).sub( ']', '' )
 				line1_flag = false
+				line2_flag = true
 			else
 				puts 'Incomplete data.'
 				exit( 0 )
 			end
+		elsif line2_flag
+			items = line.chomp.split( "\t" )
+			line2_flag = false
 		else
 			import_solid << line.force_encoding( 'utf-8' ).chomp.split( "\t" )
 		end
 	end
 end
 puts "[#{txt_class}]"
+puts items
 
 if import_solid.size == 0
 	puts 'Empty data.'
@@ -83,7 +90,11 @@ when 'dic'
 				res = $DB.query( "SELECT * FROM #{$TB_DIC} WHERE FG='#{e[0]}' AND org_name='#{e[1]}' AND alias='#{e[2]}';" )
 
 				if res.first
-					$DB.query( "UPDATE #{$TB_DIC} SET user='#{e[3]}', def_fn='#{e[4]}' WHERE FG='#{e[0]}'AND org_name='#{e[1]}' AND alias='#{e[2]}';" ) if opt == 'ow'
+					if opt == 'ow'
+						$DB.query( "UPDATE #{$TB_DIC} SET user='#{e[3]}', def_fn='#{e[4]}' WHERE FG='#{e[0]}'AND org_name='#{e[1]}' AND alias='#{e[2]}';" )
+					else
+						puts "[SKIP]#{e}"
+					end
 				else
 
 					$DB.query( "INSERT INTO #{$TB_DIC} SET FG='#{e[0]}', org_name='#{e[1]}', alias='#{e[2]}', user='#{e[3]}', def_fn='#{e[4]}';" )
@@ -99,8 +110,117 @@ when 'dic'
 	end
 	puts "#{count} data have imported."
 
+when 'fctp'
+	# user, @fct_item
+	if true #items.size == 14
+		$DB.query( "DELETE FROM #{$TB_FCTP};" ) if opt == 'xx'
+ 
+		import_solid.each do |e|
+			a = Array.new( 100 )
+			a = e.dup
+
+			print "#{count}\r"
+			begin
+				res = $DB.query( "SELECT * FROM #{$TB_FCTP} WHERE user='#{a[0]}' AND FN='#{a[2]}';" )
+				if res.first
+					if opt == 'ow'
+						query = "UPDATE #{$TB_FCTP} SET"
+						items.each.with_index do |e, i|
+							query << " #{e}='#{a[i]}'," unless e == 'SID' || i == 1 || i == 4
+						end
+						query.chop!
+						query<< " WHERE user='#{a[0]}' AND FN='#{a[2]}';"
+						$DB.query( query )
+					else
+						puts "[SKIP]#{a}"
+					end
+				else
+					query = "INSERT INTO #{$TB_FCTP} SET"
+					items.each.with_index do |e, i|
+						query << " #{e}='#{a[i]}'," unless e == 'SID'
+					end
+					query.chop!
+					query<< ";"
+
+					$DB.query( query ) unless a[2].to_s.empty?
+				end
+			rescue
+				puts "[ERROR]#{e}"
+			end
+			count += 1
+		end
+	else
+		puts 'Incomplete fctp data.'
+	end
+	puts "#{count} data have imported."
+
+when 'tagp'
+	# FG, FN, SID, SN, user, name, class1, class2, class3, tag1, tag2, tag3, tag4, tag5
+	if items.size == 14
+		$DB.query( "DELETE FROM #{$TB_TAG};" ) if opt == 'xx'
+
+		import_solid.each do |e|
+			a = Array.new( 20 )
+			a = e.dup
+
+			print "#{count}\r"
+			begin
+				res = $DB.query( "SELECT * FROM #{$TB_TAG} WHERE FN='#{a[1]}' AND user='#{a[4]}';" )
+				if res.first
+					if opt == 'ow'
+						$DB.query( "UPDATE #{$TB_TAG} SET FG='#{a[0]}', SID='#{a[2]}', SN='0', name='#{a[5]}', class1='#{a[6]}', class2='#{a[7]}', class3='#{a[8]}', tag1='#{a[9]}', tag2='#{a[10]}', tag3='#{a[11]}', tag4='#{a[12]}', tag5='#{a[13]}' WHERE FN='#{a[1]}'AND user='#{a[4]}';" )
+					else
+						puts "[SKIP]#{a}"
+					end
+				else
+					$DB.query( "INSERT INTO #{$TB_TAG} SET FG='#{a[0]}', FN='#{a[1]}', SID='#{a[2]}', SN='0', user='#{a[4]}', name='#{a[5]}', class1='#{a[6]}', class2='#{a[7]}', class3='#{a[8]}', tag1='#{a[9]}', tag2='#{a[10]}', tag3='#{a[11]}', tag4='#{a[12]}', tag5='#{a[13]}';" ) unless a[1].to_s.empty?
+				end
+			rescue
+				puts "[ERROR]#{e}"
+			end
+			count += 1
+		end
+	else
+		puts 'Incomplete tagp data.'
+	end
+	puts "#{count} data have imported."
+
+when 'extp'
+	# FN, user, gycv, allergen1, allergen2, unit, color1, color2, color1h, color2h, shun1s, shun1e, shun2s, shun2e
+	if items.size == 14
+		$DB.query( "DELETE FROM #{$TB_EXT};" ) if opt == 'xx'
+
+		import_solid.each do |e|
+			a = Array.new( 20 )
+			a = e.dup
+
+			print "#{count}\r"
+			begin
+				res = $DB.query( "SELECT * FROM #{$TB_EXT} WHERE FN='#{a[0]}' AND user='#{a[1]}';" )
+				if res.first
+					if opt == 'ow'
+						$DB.query( "UPDATE #{$TB_EXT} SET gycv='#{a[2].to_i}', allergen1='#{a[3].to_i}', allergen2='#{a[4].to_i}', unit='#{a[5]}', color1=0, color2=0, color1h=0, color2h=0, shun1s='#{a[10].to_i}', shun1e='#{a[11].to_i}', shun2s='#{a[12].to_i}', shun2e='#{a[13].to_i}' WHERE FN='#{a[0]}'AND user='#{a[1]}';" )
+					else
+						puts "[SKIP]#{a}"
+					end
+				else
+					$DB.query( "INSERT INTO #{$TB_EXT} SET FN='#{a[0]}', user='#{a[1]}', gycv='#{a[2].to_i}', allergen1='#{a[3].to_i}', allergen2='#{a[4].to_i}', unit='#{a[5]}', color1=0, color2=0, color1h=0, color2h=0, shun1s='#{a[10].to_i}', shun1e='#{a[11].to_i}', shun2s='#{a[12].to_i}', shun2e='#{a[13].to_i}';" ) unless a[0].to_s.empty?
+				end
+			rescue
+				puts "[ERROR]#{e}"
+			end
+			count += 1
+		end
+	else
+		puts 'Incomplete extp data.'
+	end
+	puts "#{count} data have imported."
+
 else
 	puts 'Importable data list..'
 	puts 'dic'
+	puts 'ftcp'
+	puts 'tagp'
+	puts 'extp'
 end
 
